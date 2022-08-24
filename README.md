61a08c3f7d97b71b54379cd385f8c6662d70dbca
21stChrom <parkerjmartin2020@gmail.com>
2022-08-24 00:36:34 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-at9jyv
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-at9jyv)
---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import StopWatch from './Components/StopWatch/StopWatch.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <div style={{color:"white"}}>
+        Parker was here!
+        </div>
+      <StopWatch />
+      <iframe id="embed" src="https://react-jhj8h9.stackblitz.io?embed=1&file=index.js, src/App.js" className="App"></iframe>
+      </div>
+  );
+  }
+
+export default App;
